c94e1e45933a8d96e12c710fe286afe87c5d3085
prasad <prasadbiradar037@gmail.com>
2019-02-07 22:01:28 +0530

    Updates

---
+++ precache-manifest.0c7be9b2040e9edff475ebf9836d39be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "40fedafdff88d1c4bfe3ccfe1149768d",
+    "url": "/prasadbiradar/simple-react-carousel/static/media/rampage.40fedafd.jpeg"
+  },
+  {
+    "revision": "239a5f9a659b78688a85337635adac84",
+    "url": "/prasadbiradar/simple-react-carousel/static/media/3.239a5f9a.jpg"
+  },
+  {
+    "revision": "35f775f779ab6ace0e42bc7a944352af",
+    "url": "/prasadbiradar/simple-react-carousel/static/media/2.35f775f7.jpeg"
+  },
+  {
+    "revision": "23eac953f48d3cedcd79",
+    "url": "/prasadbiradar/simple-react-carousel/static/js/runtime~main.23eac953.js"
+  },
+  {
+    "revision": "9e64266f599f695b0e7e",
+    "url": "/prasadbiradar/simple-react-carousel/static/js/main.9e64266f.chunk.js"
+  },
+  {
+    "revision": "bc5b8f5bdedf95012c8d",
+    "url": "/prasadbiradar/simple-react-carousel/static/js/1.bc5b8f5b.chunk.js"
+  },
+  {
+    "revision": "9e64266f599f695b0e7e",
+    "url": "/prasadbiradar/simple-react-carousel/static/css/main.2ac4c09e.chunk.css"
+  },
+  {
+    "revision": "0fe5ab1062516ba7047c18b55689d197",
+    "url": "/prasadbiradar/simple-react-carousel/index.html"
+  }
+];
